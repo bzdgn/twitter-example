d6b7e5b186cf1e41930c65a0a8668633f4f67f82
Levent Divilioglu <divilioglu@gmail.com>
2019-04-12 18:19:31 +0200

    first commit

---
+++ src/main/java/com/bzdgn/twitterex/App.java
@@ -0,0 +1,13 @@
+package com.bzdgn.twitterex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
